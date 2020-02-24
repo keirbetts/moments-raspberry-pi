fa60f5a1ebe7d0bd22ab3e178b0754b27ca80919
Dominic Hui <dominichui112@gmail.com>
2020-02-24 14:25:31 +0000

    call inside function, getURLs, and set timer to call function every 10 seconds

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 
 import urllib.request
 import boto3
+import threading
 
 # make connection to dynamo db
-client = boto3.resource('dynamodb')
-
-# connect to table
-table = client.Table("Moments-dev")
-
-# get urls
-response = table.get_item(
-    Key={"usr": "crookydan"}
-)
-
-pictureUrls = response["Item"]["picURL"]
 
 
-# iterate through and dl photos
-counter = 0
-for url in pictureUrls:
-    print(url)
-    counter += 1
-    urllib.request.urlretrieve(
-        url, "/home/domh/Pictures/temp/{}.jpg".format(counter))
+def getUrls():
 
+    threading.Timer(10.0, getUrls).start()
+    client = boto3.resource('dynamodb')
+
+    # connect to table
+    table = client.Table("Moments-dev")
+
+    # get urls
+    response = table.get_item(
+        Key={"usr": "crookydan"}
+    )
+
+    # iterate through and dl photos
+
+    pictureUrls = response["Item"]["picURL"]
+    counter = 0
+    for url in pictureUrls:
+        print(url)
+        counter += 1
+        urllib.request.urlretrieve(
+            url, "/home/domh/Pictures/temp/{}.jpg".format(counter))
+
+
+getUrls()
 
 # contact db and get list of all urls from db
-# AWS sdk - boto3
+# AWS sdk - boto3q
 # target attribute picURL
 # iterate over list and get urls
 # download each url to folder
