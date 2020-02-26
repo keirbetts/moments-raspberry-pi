14a4dcbb4195861a7d231f3c1ac570ae9ae629f6
Dominic Hui <dominichui112@gmail.com>
2020-02-26 08:45:42 +0000

    removed unneeded prints and sudo code

--- main.py
+++ main.py
@@ -5,32 +5,28 @@ import os
 from utils import getUsrPhotoUrls, downloadPhotos, slideControl
 
 
+previousUrls = []
+stock = True
+
+
 def start():
     # start slide show
     os.system('sh feh_stock.sh')
 
 
-previousUrls = []
-stock = True
-
-
 def main():
     global previousUrls
     global stock
-    # os.system('sh feh_stock.sh')
     threading.Timer(10.0, main).start()
-    print('INSIDE MAIN')
 
     # get photo urls
     currentUrls = getUsrPhotoUrls()
 
-    # if len(previousUrls) != len(currentUrls):
     if sorted(set(previousUrls)) != sorted(set(currentUrls)):
         # download photos
         downloadPhotos(previousUrls, currentUrls)
         stock = downloadPhotos(previousUrls, currentUrls)
         previousUrls = getUsrPhotoUrls()
-        print(stock)
 
         slideControl(stock)
 
