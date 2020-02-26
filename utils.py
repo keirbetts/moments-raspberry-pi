2ea00420a3739f7d5f90718b1be2112da0118649
Dominic Hui <dominichui112@gmail.com>
2020-02-26 08:46:36 +0000

    removed prints from utils module

--- utils.py
+++ utils.py
@@ -25,9 +25,7 @@ def downloadPhotos(previousUrls, currentUrls):
     additionalTotal = len(currentUrls) - len(previousUrls)
 
     if additionalTotal > 0:
-        print('ADDITIONAL URLS COMPARISON')
         for url in additionalUrls:
-            print(url, 'INSIDE THE FOR LOOP')
             counter += 1
             urllib.request.urlretrieve(
                 url, "/home/domh/Pictures/temp/{}.jpeg".format(counter))
@@ -41,10 +39,8 @@ def downloadPhotos(previousUrls, currentUrls):
 
 def slideControl(stock):
     if stock:
-        print("killing slideshow")
         os.system('sh kill.sh')
         os.system('sh feh_stock.sh')
     elif stock == False:
-        print("killing slid show, starting stock")
         os.system('sh kill.sh')
         os.system('sh script_slideshow.sh')
